36cdc114167ba9a4d8a02cfb92bb9eb584336fc3
Xen0Xys <red57101@gmail.com>
2024-06-29 00:15:58 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Eventoria
+
+## Table of content
+- [Road map](#road-map)
+    - [Authentication System](#authentication-system)
+    - [Pages & events](#pages--events)
+    - [Notifications](#notifications)
+
+## Road map
+All tasks must be delivered before or by the given deadline. Any delay will be followed by a belt-tightening by ALL team members (target excluded).
+
+### Authentication System
+**Deadlines**:
+- Front-End: `30 July 2024`
+- Back-End: `12 July 2024`
+
+> **Notes**:
+> Think to handle e-mail sending.
+
+| Feature                | Done |
+|:-----------------------|:----:|
+| Banned emails          |  ❌   |
+| User registration      |  ❌   |
+| Authentication         |  ❌   |
+| OTP                    |  ❌   |
+| Reset password         |  ❌   |
+| Settings: User name    |  ❌   |
+| Settings: Display name |  ❌   |
+| Settings: Password     |  ❌   |
+| Settings: Avatar       |  ❌   |
+
+### Pages & events
+**Deadlines**:
+- Front-End: `?`
+- Back-End: `?`
+
+> **Notes**:
+> Think to send notifications.
+
+| Feature                 | Done |
+|:------------------------|:----:|
+| Page management         |  ❌   |
+| Commentary (pages)      |  ❌   |
+| Page reports            |  ❌   |
+| Admin actions on page   |  ❌   |
+| ---                     | ---  |
+| Event management        |  ❌   |
+| Commentary (events)     |  ❌   |
+| Event report            |  ❌   |
+| Participation indicator |  ❌   |
+| Admin actions on event  |  ❌   |
+
+### Notifications
+**Deadlines**:
+- Front-End: `?`
+- Back-End: `?`
+
+| Feature                   | Done |
+|:--------------------------|:----:|
+| Different types           |  ❌   |
+| Detect when not read      |  ❌   |
+| Badge of amount on unread |  ❌   |
---
+++ src/common/middlewares/logger.middleware.ts
@@ -0,0 +1,61 @@
+import {Injectable, Logger, NestMiddleware} from "@nestjs/common";
+import {FastifyReply, FastifyRequest} from "fastify";
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware{
+
+    static logger: Logger = new Logger(LoggerMiddleware.name);
+
+    use(req: FastifyRequest["raw"], res: FastifyReply["raw"], next: () => void){
+        const startTime = Date.now();
+        res.on("finish", () => {
+            const path = req.url;
+            try{
+                let httpOrHttps;
+                if(!req.connection.localPort)
+                    httpOrHttps = "H2";
+                else
+                    httpOrHttps = req.connection.localPort.toString() === process.env.HTTPS_PORT ? "HTTPS" : "HTTP";
+                const method = req.method;
+                if(method === "OPTIONS")
+                    return;
+                const statusCode = res.statusCode;
+                const duration = Date.now() - startTime;
+                const resSize: any = res.getHeader("Content-Length") || "0";
+                // const nRes = res as any;
+                // const resSize = nRes._contentLength || "0";
+                const intResSize = parseInt(resSize);
+                LoggerMiddleware.logger.log(`${httpOrHttps} ${method} ${path} ${statusCode} ${duration}ms ${intResSize}`);
+                LoggerMiddleware.requestTimeLogger(path, method, duration);
+            }catch(e){
+                LoggerMiddleware.logger.warn(`Can't log route ${path} : ${e}`);
+            }
+        });
+        next();
+    }
+
+    static requestTimeLogger(path: string, method: string, ms: number){
+        switch (method){
+            case "GET":
+                if(ms > 750)
+                    LoggerMiddleware.logger.warn(`GET (${path}) request took more than 750ms (${ms}ms)`);
+                break;
+            case "POST":
+                if(ms > 1500)
+                    LoggerMiddleware.logger.warn(`POST (${path}) request took more than 1500ms (${ms}ms)`);
+                break;
+            case "PUT":
+                if(ms > 1500)
+                    LoggerMiddleware.logger.warn(`PUT (${path}) request took more than 1500ms (${ms}ms)`);
+                break;
+            case "PATCH":
+                if(ms > 500)
+                    LoggerMiddleware.logger.warn(`PATCH (${path}) request took more than 500ms (${ms}ms)`);
+                break;
+            case "DELETE":
+                if(ms > 500)
+                    LoggerMiddleware.logger.warn(`DELETE (${path}) request took more than 500ms (${ms}ms)`);
+                break;
+        }
+    }
+}
