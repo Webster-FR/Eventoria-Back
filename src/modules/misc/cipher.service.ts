09fb8569c5c199ca63814e77c684834b9f1582b7
Xen0Xys <red57101@gmail.com>
2024-07-08 13:48:42 +0200

    :sparkles: Add cipher service

--- src/modules/misc/cipher.service.ts
+++ src/modules/misc/cipher.service.ts
@@ -106,8 +106,14 @@ export class CipherService{
             }, buffer).toString("utf-8");
     }
 
-    // Secret functions
-    generateSecret(length = 32){
-        return crypto.randomBytes(length).toString("hex");
+    generateRandomBytes(bytesNumber = 32){
+        return crypto.randomBytes(bytesNumber).toString("hex");
+    }
+
+    generateRandomNumbers(numbersNumber = 6){
+        let numbers = "";
+        for(let i = 0; i < numbersNumber; i++)
+            numbers += Math.floor(Math.random() * 10);
+        return numbers;
     }
 }
