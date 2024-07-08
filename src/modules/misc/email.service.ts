c0ffed9264e6283525077709c52841a00eceadca
Xen0Xys <red57101@gmail.com>
2024-07-08 16:18:24 +0200

    :ambulance: Fix email sending

--- src/modules/misc/email.service.ts
+++ src/modules/misc/email.service.ts
@@ -1,18 +1,20 @@
 import {Injectable} from "@nestjs/common";
 import {MailerService} from "@nestjs-modules/mailer";
+import {ConfigService} from "@nestjs/config";
 
 @Injectable()
 export class EmailService{
     constructor(
         private readonly mailerService: MailerService,
+        private readonly configService: ConfigService,
     ){}
 
     async sendEmail(to: string, subject: string, body: string){
         await this.mailerService.sendMail({
-            from: "Eventoria",
+            from: this.configService.get("EMAIL_USER"),
             to,
             subject,
-            html: body,
+            text: body,
         });
     }
 }
